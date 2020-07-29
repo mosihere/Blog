d06d6008c65a69e8d7e195b77cc22da28ab69b3a
mosihere <mostafakhoshhal@yahoo.com>
2020-07-29 19:49:44 +0430

    logout and login completed

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-
+from django.contrib.auth import login, logout
 
 # Create your views here.
 
@@ -9,9 +9,12 @@ def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            # TODO: Users Login
+            user = form.save()
+            login(request, user)
+
             return redirect('articles:list')
+
+
 
 
 # Else --> ( if method was get ) show a empty list to user
@@ -27,7 +30,10 @@ def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(data = request.POST)
         if form.is_valid():
-            # TODO: Login User
+            # if it was valid -->
+            user = form.get_user()
+            login(request, user)
+
             return redirect('articles:list')
 
 
@@ -35,3 +41,12 @@ def login_view(request):
         form = AuthenticationForm()
 
     return render(request, 'accounts/login.html', {'form':form})
+
+
+
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+
+        return redirect('articles:list')
